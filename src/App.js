fc5e1d4c978bf09609ab200c75f002810ec23e8a
dev-chloe <chloekim.4969@gmail.com>
2021-05-11 23:47:49 +0900

    react200 초급021

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import './App.css';
-import PropsObjVal from './R020_PropsObjVal';
+import PropsRequired from './R021_PropsRequired';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>HTML, CSS 적용하기</p>
-      <PropsObjVal ObjectJson = {{react: "리액트", twohundred: 200}}/>
-      {/* 
-      <PropsBoolean BooleanTrueFalse={false} />
-      <PropsBoolean BooleanTrueFalse />
-      
-      <PropsDatatype
-        String = "react"
-        Number = { 200 }
-        Boolean = { 1==1 }
-        Array = { [0, 1, 8]}
-        ObjectJson = {{react: "리액트", twoHundred: '200'}}
-        Function = {console.log("FunctionProps: function!")}
-      /> */}
+      <PropsRequired
+        ReactNumber = {200}/>
     </div>
   );
 }
