d8cd3609d633fa8d2369e0428f5008a612a768be
dev-chloe <chloekim.4969@gmail.com>
2021-05-21 22:28:57 +0900

    react200 초급044

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import ReactstrapFade from './R043_ReactstrapFade';
+import ReactstrapForm from './R044_ReactstrapForm';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div>
+    <div className="cont_box">
       <h1>Start React 200!</h1>
       <p>HTML, CSS 적용하기</p>
-      <ReactstrapFade />
+      <ReactstrapForm />
     </div>
   );
 }
