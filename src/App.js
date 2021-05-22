1d44c7ea517b2bdb7194d567d70687ca308da3b3
dev-chloe <chloekim.4969@gmail.com>
2021-05-22 23:22:36 +0900

    react200 초급051

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ReactstrapPagination from './R050_ReactstrapPagination';
+import ReactstrapPopover from './R051_ReactstrapPopover';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="cont_box">
       <h1>Start React 200!</h1>
       <p>HTML, CSS 적용하기</p>
-      <ReactstrapPagination />
+      <ReactstrapPopover />
     </div>
   );
 }
