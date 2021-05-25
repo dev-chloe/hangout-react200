7ec26cd63db281fc84217aa390a89df6ee26eb55
dev-chloe <chloekim.4969@gmail.com>
2021-05-25 22:43:26 +0900

    react200 초급057

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Sweetalert2Basic from './R056_Sweetalert2Basic';
+import Sweetalert2Position from './R057_Sweetalert2Position';
 
 function App() {
   return (
     <div className="cont_box">
       <h1>Start React 200!</h1>
       <p>HTML, CSS 적용하기</p>
-      <Sweetalert2Basic />
+      <Sweetalert2Position />
     </div>
   );
 }
