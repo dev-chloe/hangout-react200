92c4522c4cf0d27166fbb0650d7cce9b56f983ed
dev-chloe <chloekim.4969@gmail.com>
2021-05-31 23:16:09 +0900

    react200 중급068 fixed

--- src/R068_onMouseMove.js
+++ src/R068_onMouseMove.js
@@ -5,6 +5,10 @@ class R068_onMouseMove extends Component {
     console.log('TAG : ' + tag);
   }
 
+  // MouseMove 함수 선언후 전달받은 파라미터를 tag라는 함수의 내부 변수에 넣어 사용한다.
+  // tag 변수를 로그로 출력한다.
+  // <div> 태그 영역에서 마우스 커서가 움직일 경우 MouseMove 함수를 호출하고 파라미터로 넘긴 div가 출력된다.
+
   render() {
     return (
       <>
