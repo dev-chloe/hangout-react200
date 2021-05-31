6a4f979518c730bce296ab5dc13447d141e35354
dev-chloe <chloekim.4969@gmail.com>
2021-05-31 22:36:49 +0900

    react200 중급068

--- src/R067_onChange.js
+++ src/R067_onChange.js
@@ -6,6 +6,13 @@ class R067_onChange extends Component {
     console.log('param : ' + val);
   }
 
+  // change 라는 함수를 선언하고 전달받은 파라미터를 e라는 함수의 내부 변수에 넣어 사용한다.
+  // 이벤트 객체 변수 e에 target 속성을 붙여 value값을 가져오고 val 변수에 할당한다.
+  //  val 변수를 로그로 출력한다.
+  // input 박스에 문자를 입력 및 삭제 할때마다 onChange 이벤트가 발생해 change 함수를 호출한다.
+  // 함수 호출 시 파라미터를 따로 넘겨 주지 않아도 이벤트 객체가 전달된다.
+  // select 박스 선택된 option이 달라질때마다 onChange 이벤트가 발생해 change 함수를 호출한다
+
   render() {
     return (
       <>
