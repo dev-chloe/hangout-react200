03463c068a794daf50b8069f50efc19613cccd9a
dev-chloe <chloekim.4969@gmail.com>
2021-07-22 22:32:03 +0900

    react 실무 126 react 조회 페이지 만들기(4)

--- client/src/component/SoftwareToolsManage/SoftwareList.js
+++ client/src/component/SoftwareToolsManage/SoftwareList.js
@@ -7,8 +7,8 @@ class SoftwareList extends Component {
       super(props);
 
       this.state = {
-          responseSwtoolList: '',
-          append_SwtoolList: '',
+        responseSwtoolList: '',
+        append_SwtoolList: '',
       }
   }
 
@@ -22,12 +22,8 @@ class SoftwareList extends Component {
     .then( response => {
       // console.log(response)
       try {
-        this.setState({ 
-          responseSwtoolList: response 
-        },);
-        this.setState({
-          append_SwtoolList: this.SoftwareListAppend() 
-        })
+        this.setState({ responseSwtoolList: response });
+        this.setState({ append_SwtoolList: this.SwToolListAppend() });
       } catch (error) {
         alert ('리액트 스테이트 변경 중 에러가 발생했습니다.');
       }
@@ -39,55 +35,64 @@ class SoftwareList extends Component {
   }
 
   SwToolListAppend = () => {
-    let result = [];
-    var SwToolList = this.state.responseSwtoolList;
-    console.log(SwToolList)
-    for(let i=0; i<SwToolList.json.length; i++) {
-      var data = SwToolList.json[i];
+    // let result = []
+    var SwToolList = this.state.responseSwtoolList.data;
+    // console.log(SwToolList)
+    
+    // for(let i=0; i<SwToolList.json.length; i++){
+    //     var data = SwToolList.json[i]
 
-      var date = date.reg_date;
-      var year = date.substr(0,4);
-      var month = date.substr(4,2);
-      var day = date.substr(6,2);
-      var reg_date = `${year}.${month}.${day}`;
+    //     var date = data.reg_date
+    //     var year = date.substr(0,4)
+    //     var month = date.substr(4,2)
+    //     var day = date.substr(6,2)
+    //     var reg_date = year +'.'+month+'.'+day
 
-      result.push(
-        <tr className="hidden_type">
-          <td>{data.swt_toolname}</td>
-          <td>{data.swt_function}</td>
-          <td>{reg_date}</td>
-          <td>
-            <Link to={'/AdminSoftwareView'+data.swt_code} className="bt_cl bt_c2 w50_b">수정</Link>
-            <a href="#n" class="bt_c1 w50_b">삭제</a>
-          </td>
-        </tr>
-      )
-    }
-    return result
+    //     result.push(
+    //         <tr className="hidden_type">
+    //             <td>{data.swt_toolname}</td>
+    //             <td>{data.swt_function}</td>
+    //             <td>{reg_date}</td>
+    //             <td>
+    //                 <Link to={'/AdminSoftwareView/'+data.swt_code} 
+    //                 className="bt_c1 bt_c2 w50_b">수정</Link>
+    //                 <a href="#n" className="bt_c1 w50_b" >삭제</a>
+    //             </td>
+    //         </tr>
+    //     )
+    // }
+    return SwToolList
   }
 
-  render() {
+  render () {
     return (
-      <section className="sub_wrap">
-        <article className="s_cnt mp_pro_li cti mp_pro_li_admin">
+      <section className="sub_wrap" >
+        <article className="s_cnt mp_pro_li ct1 mp_pro_li_admin">
+          <div className="li_top">
+              <h2 className="s_tit1">Software Tools 목록</h2>
+              <div className="li_top_sch af">
+              <Link to={'/AdminSoftwareView/register'} className="sch_bt2 wi_au">Tool 등록</Link>
+              </div>
+          </div>
+
           <div className="list_cont list_cont_admin">
-            <table>
+            <table className="table_ty1 ad_tlist">
               <tr>
-                <th>
-                  툴 이름
-                </th>
+                <th>툴 이름</th>
                 <th>기능</th>
                 <th>등록일</th>
                 <th>기능</th>
               </tr>
-            </table>
-            <table>
-              {this.state.append_SwtoolList}
+            </table>	
+            <table className="table_ty2 ad_tlist">
+              <tr>
+                <td>{this.state.append_SwtoolList.swt_toolname}</td>
+              </tr>
             </table>
           </div>
         </article>
       </section>
-    )
+    );
   }
 }
 
